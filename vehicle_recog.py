e15c6cda186bb0a66cca1ba8457e306ff8b350b0
root <yangshengkai@huawei.com>
2018-05-14 20:56:32 +0800

    m

--- vehicle_recog.py
+++ vehicle_recog.py
@@ -2,6 +2,7 @@
 import TencentYoutuyun
 import json
 import sys,os
+import cv2
 # -*- coding: utf-8 -*-
 appid='10130422'
 secret_id = 'AKID7CD6rYXXoyyoU3fpEVcKyOjuBTuI8qzx'
@@ -31,10 +32,11 @@ f.close()
 print(len(list_f))
 
 
-#f=open('result.json','wb+')
+log_err=open('log_err','wb+')
+log_warning=open('log_warning','wb+')
 
-#for i in range(0,1):
-for i in range(0,len(list_f)):
+for i in range(0,20):
+#for i in range(0,len(list_f)):
 	ret = youtu.carclassify(list_f[i],0)
 	print(list_f[i])
 	filename_start=list_f[i].rfind("/")+1
@@ -47,6 +49,23 @@ for i in range(0,len(list_f)):
 	f.write(jsret.encode('raw_unicode_escape').decode('utf8').encode('utf8'))
 #	f.write("\n")
 	f.close()
+	if(ret['errorcode']!=0):
+		log_err.write("[ERROR]:"+list_f[i])
+		continue
+	if(len(ret['tags'])==0):
+		log_warning.write(list_f[i])
+		continue
+	y=int(ret['car_coord']['x'])
+	x=int(ret['car_coord']['y'])
+	h=int(ret['car_coord']['height'])
+	w=int(ret['car_coord']['width'])
+	color=ret['tags'][0]['color']
+	image=cv2.imread(list_f[i])
+	x2=x+h
+	y2=y+w
+	crop=image[x:x2,y:y2]
+	filename="./jpg_cropped/"+list_f[i][filename_start:]+".bmp"
+	cv2.imwrite(filename,crop)
 
 
 #f.close()
