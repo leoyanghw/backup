60fe50c728ce580c6b3de5e426ff5543d096feb6
root <yangshengkai@huawei.com>
2018-05-14 20:11:17 +0800

    modify

--- vehicle_recog.py
+++ vehicle_recog.py
@@ -2,6 +2,7 @@
 import TencentYoutuyun
 import json
 import sys,os
+# -*- coding: utf-8 -*-
 appid='10130422'
 secret_id = 'AKID7CD6rYXXoyyoU3fpEVcKyOjuBTuI8qzx'
 secret_key = 'ymZI856HIXgfT9e1pcImBSYo0Uq482qU'
@@ -30,12 +31,22 @@ f.close()
 print(len(list_f))
 
 
-f=open('result.txt','wb+')
+#f=open('result.json','wb+')
 
+#for i in range(0,1):
 for i in range(0,len(list_f)):
 	ret = youtu.carclassify(list_f[i],0)
 	print(list_f[i])
+	filename_start=list_f[i].rfind("/")+1
+	filename="./output_json/"+list_f[i][filename_start:]+".json"
+#	filename="./output_json/"+str(i)+".json"
+#	print(filename)
+	f=open(filename,'wb+')
 	jsret = json.dumps(ret,ensure_ascii=False,encoding='utf-8',indent=2)
 	#print jsret.encode('raw_unicode_escape').decode('utf8')
 	f.write(jsret.encode('raw_unicode_escape').decode('utf8').encode('utf8'))
-f.close()
+#	f.write("\n")
+	f.close()
+
+
+#f.close()
