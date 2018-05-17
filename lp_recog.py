6388db02f93272fa3a9192b4db02dadfdba5e9af
root <yangshengkai@huawei.com>
2018-05-17 13:44:05 +0800

    modify

--- lp_recog.py
+++ lp_recog.py
@@ -31,16 +31,19 @@ file_list(path,f,list_f)
 f.close()
 print(len(list_f))
 
+plate_number=open("plate_number","wb+")
 
 log_err=open('lp_log_err','wb+')
 log_warning=open('lp_log_warning','wb+')
 
-#for i in range(0,20):
+#for i in range(0,2):
 for i in range(0,len(list_f)):
 	ret = youtu.plateocr(list_f[i],0)
 	print(list_f[i])
 	filename_start=list_f[i].rfind("/")+1
 	filename="./lp_output_json/"+list_f[i][filename_start:]+".json"
+	fname=list_f[i][filename_start:]
+	fname=fname.strip("\.bmp")
 #	print(filename)
 	f=open(filename,'wb+')
 	jsret = json.dumps(ret,ensure_ascii=False,encoding='utf-8',indent=2)
@@ -58,7 +61,8 @@ for i in range(0,len(list_f)):
 	x=int(ret['items'][0]['itemcoord']['y'])
 	h=int(ret['items'][0]['itemcoord']['height'])
 	w=int(ret['items'][0]['itemcoord']['width'])
-	color=ret['items'][0]['itemstring']
+	plate=ret['items'][0]['itemstring']
+	plate_number.write(fname+','+plate.encode('raw_unicode_escape').decode('utf8').encode('utf8')+'\n')
 	image=cv2.imread(list_f[i])
 	x2=x+h
 	y2=y+w
