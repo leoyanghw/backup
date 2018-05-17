01af9e35630114529ba1e0ac5be508da4a358fbe
root <yangshengkai@huawei.com>
2018-05-17 10:36:53 +0800

    add image_attr

--- vehicle_recog.py
+++ vehicle_recog.py
@@ -32,14 +32,17 @@ f.close()
 print(len(list_f))
 
 
+
+csv=open('image_attr.csv','wb+')
 log_err=open('log_err','wb+')
 log_warning=open('log_warning','wb+')
 
-#for i in range(0,20):
+#for i in range(0,2):
 for i in range(0,len(list_f)):
 	ret = youtu.carclassify(list_f[i],0)
 	print(list_f[i])
 	filename_start=list_f[i].rfind("/")+1
+	fname=list_f[i][filename_start:]
 	filename="./output_json/"+list_f[i][filename_start:]+".json"
 #	filename="./output_json/"+str(i)+".json"
 #	print(filename)
@@ -59,8 +62,10 @@ for i in range(0,len(list_f)):
 	x=int(ret['car_coord']['y'])
 	h=int(ret['car_coord']['height'])
 	w=int(ret['car_coord']['width'])
-	color=ret['tags'][0]['color']
+	v_type=ret['tags'][0]['type']
+	v_color=ret['tags'][0]['color']
 	image=cv2.imread(list_f[i])
+	csv.write(fname+","+v_type.encode('raw_unicode_escape').decode('utf8').encode('utf8')+','+v_color.encode('raw_unicode_escape').decode('utf8').encode('utf8')+"\n")
 	x2=x+h
 	y2=y+w
 	crop=image[x:x2,y:y2]
@@ -68,4 +73,4 @@ for i in range(0,len(list_f)):
 	cv2.imwrite(filename,crop)
 
 
-#f.close()
+csv.close()
